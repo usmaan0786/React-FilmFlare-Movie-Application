0a1510aac628bbc8c10c01bda375d9226e97e2be
Usman Manzoor <usmanmanzoor1000@gmail.com>
2023-12-13 22:56:20 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Usman</h1>
+  </React.StrictMode>
+);
+
